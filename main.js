27b87055276c48eec73911f8c3581e177a3394e5
Matt Plunkett <m.g.plunkett@gmail.com>
2024-12-31 22:23:35 +0000

    add github repo updated date

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const myEmail = "m.g.plunkett@gmail.com";
-let email = document.getElementById("copy-email");
+
+const email = document.getElementById("copy-email");
 
 email.addEventListener("click", copyEmail);
 
@@ -16,3 +17,31 @@ function copyEmail() {
     }
   );
 }
+
+const repoOwner = "magepl";
+const repoName = "portfolio-2025";
+const url = `https://api.github.com/repos/${repoOwner}/${repoName}`;
+const repoDateUpdated = document.getElementById("github-last-updated-at");
+
+fetch(url)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
+  })
+  .then((data) => {
+    const date = new Date(data.updated_at);
+    const [day, month, year] = [
+      date.getDate(),
+      date.getMonth(),
+      date.getFullYear(),
+    ];
+    repoDateUpdated.textContent = `Last updated on: ${day}/${
+      month + 1
+    }/${year}`;
+  })
+  .catch((error) => {
+    console.error(`Error: ${error.message}`);
+  });
